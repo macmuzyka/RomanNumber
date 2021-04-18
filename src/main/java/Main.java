c47f34cadb552d4d729e22ca99f0c4f613ada4ab
macmuzyka <raweshau@gmail.com>
2021-04-18 23:56:20 +0200

    main class for user input

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * Created by Maciej Muzyka
+ * on 18.04.2021
+ * TODO:
+ */
+
+class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean quit = false;
+        while(!quit) {
+            try {
+                System.out.println("Enter the number to be converted to its Roman representation:");
+                int input = scanner.nextInt();
+                RomanConverter converter = new RomanConverter();
+                String romanSymbol = converter.convertToRoman(input);
+                System.out.println(romanSymbol);
+                quit = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+}
