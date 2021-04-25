b52b9b902364485ad7185d7833e35b801ef693db
macmuzyka <raweshau@gmail.com>
2021-04-25 09:38:51 +0200

    custom exception for roman converter

---
+++ src/main/java/OutOfBoundsException.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Maciej Muzyka
+ * on 25.04.2021
+ */
+
+class OutOfBoundsException extends RuntimeException{
+    OutOfBoundsException(final String message) {
+        super(message);
+    }
+}
